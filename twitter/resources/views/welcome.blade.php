7910872d8ea65fc792815310b1484ab331d4613c
ally <ec2-user@ip-172-31-31-166.us-east-2.compute.internal>
2018-06-18 05:07:37 +0000

    init

--- twitter/resources/views/welcome.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('layouts.app')
-
-@section('content')
-    <div class="center jumbotron">
-        <div class="text-center">
-            <h1>Welcome to the Kadaitasklist</h1>
-        </div>
-    </div>
-@endsection

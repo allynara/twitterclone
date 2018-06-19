ea20adb8a82cc0885c666e458370b94e8c809a37
ally <ec2-user@ip-172-31-31-166.us-east-2.compute.internal>
2018-06-19 01:35:19 +0000

    user pages

--- microposts/app/Http/Controllers/UsersController.php
+++ microposts/app/Http/Controllers/UsersController.php
@@ -8,12 +8,20 @@ use App\User; // add
 
 class UsersController extends Controller
 {
-        public function index()
+       public function index()
     {
         $users = User::paginate(10);
         
         return view('users.index', [
             'users' => $users,
+        ]);
+    }
+      public function show($id)
+    {
+        $user = User::find($id);
+
+        return view('users.show', [
+            'user' => $user,
         ]);
     }
 }
